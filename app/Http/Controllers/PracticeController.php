7628ed8f58fcacf5a74cb232d30eeaab6a166b72
Mildred Fakoya <luvlyayee@gmail.com>
2018-04-17 13:33:35 -0400

    update after week 11

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -8,11 +8,69 @@ use App;
 use Debugbar;
 use IanLChapman\PigLatinTranslator\Parser;
 use Carbon\Carbon;
+use App\Book;
 
 class PracticeController extends Controller
 {
 
+    public function practice12(){
+        $result = Book::where('author', '=', 'J.K. Rowling')->delete();
+        dump($result);
+    }
+    public function practice11()
+    {
+        # First get a book to delete
+        $book = Book::where('author', '=', 'F. Scott Fitzgerald')->first();
 
+        if (!$book) {
+            dump('Did not delete- Book not found.');
+        } else {
+            $book->delete();
+            dump('Deletion complete; check the database to see if it worked...');
+        }
+    }
+    public function practice10()
+    {
+        # First get a book to update
+        $book = Book::where('author', '=', 'F. Scott Fitzgerald')->first();
+
+        if (!$book) {
+            dump("Book not found, can't update.");
+        } else {
+            # Change some properties
+            $book->title = 'The Really Great Gatsby';
+            $book->published_year = '2025';
+
+            # Save the changes
+            $book->save();
+
+            dump('Update complete; check the database to confirm the update worked.');
+        }
+    }
+    public function practice8()
+    {
+        $book = new Book();
+        $books = $book->where('title', 'LIKE', '%Harry Potter%')->get();
+
+        if ($books->isEmpty()) {
+            dump('No matches found');
+        } else {
+            foreach ($books as $book) {
+                dump($book->title);
+            }
+        }
+    }
+    public function practice7(){
+        $book = new Book();
+        $book->title = 'Harry Potter and the Sorcerer\'s Stone';
+        $book->author = 'J.K. Rowling';
+        $book->published_year = 1997;
+        $book->cover_url = 'http://prodimage.images-bn.com/pimages/9780590353427_p0_v1_s484x700.jpg';
+        $book->purchase_url = 'http://www.barnesandnoble.com/w/harry-potter-and-the-sorcerers-stone-j-k-rowling/1100036321?ean=9780590353427';
+        $book->save();
+
+        dump('Added: '.$book->title);
+    }
 
 
     public function practice5()
